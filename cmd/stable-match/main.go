064817e625e3f6eb1d4e04aabfe4badb38344b34
Hofled <ofri.herold@gmail.com>
2022-03-06 02:58:55 +0200

    Merged code from visualization, and adapted it to command-line only usage

--- cmd/stable-match/main.go
+++ cmd/stable-match/main.go
@@ -11,24 +11,24 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 	"time"
 
 	"github.com/Hofled/stable-matching-go/internal/app/algorithm"
-	"github.com/Hofled/stable-matching-go/internal/app/server"
 	"github.com/Hofled/stable-matching-go/internal/app/types"
-	socketio "github.com/googollee/go-socket.io"
 )
+
+const default_group_size = 5
 
 var (
 	Verbose      bool
+	GroupSize    int
 	UnmarriedMen *list.List
 	Women        []*types.Woman
 )
 
 func init() {
 	flag.BoolVar(&Verbose, "v", false, "Whether to print the generated groups info")
+	flag.IntVar(&GroupSize, "n", default_group_size, "The group size for each gender")
 }
 
 func TrackTime() func() time.Duration {
@@ -42,44 +42,24 @@ func TrackTime() func() time.Duration {
 func main() {
 	flag.Parse()
 
-	// server socket-io server
-	s := server.Serve(8000)
-	defer s.Close()
-
 	// setup handlers
-	server.SetupHandler(s, "/", "generate", func(c socketio.Conn, groupSize int) {
-		var unmarriedMenSlice []*types.Man
-		UnmarriedMen, unmarriedMenSlice, Women = algorithm.GenerateGroups(groupSize, Verbose)
-		server.UpdatePeople(s, unmarriedMenSlice, Women)
-	})
-	server.SetupHandler(s, "/", "stable-match", func(c socketio.Conn) {
-		fmt.Println("Starting matching process...")
-		// start measuring time
-		endTrackingFunc := TrackTime()
-		history := algorithm.StableMatch(UnmarriedMen, Women)
-		c.Emit("stable-match-history", history)
-		// stop meassuring time
-		duration := endTrackingFunc()
-		// emit duration of time
-		c.Emit("stable-match-duration", duration.String())
+	UnmarriedMen, _, Women = algorithm.GenerateGroups(GroupSize, Verbose)
+	fmt.Println("Starting matching process...")
+	// start measuring time
+	endTrackingFunc := TrackTime()
+	algorithm.StableMatch(UnmarriedMen, Women)
+	// stop meassuring time
+	duration := endTrackingFunc()
 
-		log.Println(fmt.Sprintf("%s took %s", "stable matching", duration))
+	log.Println(fmt.Sprintf("%s took %s", "stable matching", duration))
 
-		if Verbose {
-			// print all matches after the stable matching completed
-			fmt.Println("Matches:")
-			fmt.Println("W <=> M")
-			fmt.Println("=======")
-			for _, w := range Women {
-				fmt.Printf("%d <=> %d\n", w.ID, w.Husband.ID)
-			}
+	if Verbose {
+		// print all matches after the stable matching completed
+		fmt.Println("Matches:")
+		fmt.Println("W <=> M")
+		fmt.Println("=======")
+		for _, w := range Women {
+			fmt.Printf("%d <=> %d\n", w.ID, w.Husband.ID)
 		}
-
-	})
-
-	// continue running so that the server won't shut down until signaled
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
-	<-c
+	}
 }
